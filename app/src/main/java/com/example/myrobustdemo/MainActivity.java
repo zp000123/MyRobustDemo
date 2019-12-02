1bb5a699869dc4dde28bbc602dfe36078b42a8a6
zp000123 <1905185456@qq.com>
2019-12-02 16:11:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myrobustdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myrobustdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
