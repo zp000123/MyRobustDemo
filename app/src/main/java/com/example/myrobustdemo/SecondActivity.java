674d7e1a3c379941ce9646cbfa50d1eba2e75b44
zp000123 <1905185456@qq.com>
2019-12-03 13:05:02 +0800

    修改后的robust

--- app/src/main/java/com/example/myrobustdemo/SecondActivity.java
+++ app/src/main/java/com/example/myrobustdemo/SecondActivity.java
@@ -1,14 +1,27 @@
 package com.example.myrobustdemo;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.meituan.robust.patch.annotaion.Modify;
+
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
+    @Modify
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initView();
+        ((TextView) findViewById(R.id.tv_hint)).setText("Bug Fix2222");
+    }
+
+    @Modify // 修复的时候需要把 private 的方法改成 public 的
+    public void initView() {
+        ((TextView) findViewById(R.id.tv_hint)).setText("Bug");
+        ((TextView) findViewById(R.id.tv_hint)).setText("Bug Fix ");
     }
 }
